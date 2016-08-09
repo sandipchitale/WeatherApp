66aeb42757afe6500cbe37bffcdb7b85632146fd
Sandip Chitale <sandipchitale@gmail.com>
2016-08-09 07:32:57 -0700

    Make sidenav work. Also support close.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MdToolbar } from '@angular2-material/toolbar';
+import { MdButton } from '@angular2-material/button';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 
 @Component({
@@ -8,19 +11,35 @@ import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
   selector: 'weather-app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ MdToolbar, MD_CARD_DIRECTIVES, MdIcon ],
+  directives: [ MD_SIDENAV_DIRECTIVES, MD_LIST_DIRECTIVES, MdToolbar, MdButton, MdIcon, MD_CARD_DIRECTIVES ],
   providers: [ MdIconRegistry ]
 })
 export class AppComponent {
   title = 'Weather App';
   weekDays = ['Mon' , 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   places = [
-    {city: 'Pleasanton', stateAndCountry: 'California, USA'},
-    {city: 'Nagpur', stateAndCountry: 'Maharashtra, India'},
-    {city: 'San Diego', stateAndCountry: 'California, USA'}
+    {city: 'Pleasanton', stateAndCountry: 'California, USA', forecast: {}},
+    {city: 'Nagpur', stateAndCountry: 'Maharashtra, India', forecast: {}},
+    {city: 'San Diego', stateAndCountry: 'California, USA', forecast: {}}
   ];
+
+  ngOnInit() {
+    for (let place of this.places) {
+      for (let weekDay of this.weekDays) {
+        place.forecast[weekDay] = this.randomTemperature();
+      }
+    }
+  }
 
   private randomTemperature(): string {
     return (20 + (Math.random() * 100)).toFixed(0);
+  }
+
+  exit() {
+    (<any>window).remote.getCurrentWindow().close();
+  }
+
+  get electronic() {
+    return !!(window && (<any>window).process);
   }
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -23,6 +23,7 @@ const materialPkgs:string[] = [
   'card',
   'toolbar',
   'sidenav',
+  'list',
   'icon'
 ];
 
